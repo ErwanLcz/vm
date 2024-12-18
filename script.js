3712aed91d1ee86ad57dd54939e5d27570e07c8f
EzYouBy1 <erwan05.yt@gmail.com>
2024-12-18 18:49:17 +0100

    all

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', highlightActiveLink);
 });
+
+
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    dropdown.addEventListener('mouseenter', () => {
+        dropdown.querySelector('.dropdown-menu').style.display = 'block';
+    });
+    dropdown.addEventListener('mouseleave', () => {
+        dropdown.querySelector('.dropdown-menu').style.display = 'none';
+    });
+});
